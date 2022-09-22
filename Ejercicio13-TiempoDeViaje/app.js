9e301d58d44163673742521303653e51ff3cd4fc
Guadalupe Abril Medina <111397971+LuAMedina@users.noreply.github.com>
2022-09-22 16:16:44 -0300

    Add files via upload

--- Ejercicio13-TiempoDeViaje/app.js
+++ Ejercicio13-TiempoDeViaje/app.js
@@ -1,4 +1,8 @@
-let distancia = prompt("Ingrese la distancia");
-let velocidad = prompt("Ingrese la velocidad");
-tiempo = distancia / velocidad
-alert(`El tiempo estimado es ${tiempo}`)
+let distancia = prompt("Ingrese la cantidad de kilómetros de distancia");
+let velocidadPie = 5
+let velocidadBici = 10
+let velocidadAuto = 60
+tiempoPie = distancia / velocidadPie
+tiempoBici = distancia / velocidadBici
+tiempoAuto = distancia / velocidadAuto
+alert(`El tiempo estimado es ${tiempoPie} horas a pie, ${tiempoBici} horas en bicicleta y ${tiempoAuto} horas en auto`)
